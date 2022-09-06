16b958c37661ea0a7281f38d3bb7050f907da5ac
Naweedzone <92320399+naweedzone@users.noreply.github.com>
2022-09-06 18:16:16 -0300

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -1,12 +1,14 @@
 let deuda = [];
+let deudaTotal = 0;
+const deudas = [];
 let nombres = [];
 let nombre = 0;
 let edad = 0;
 let dni = [];
 let edades = [];
-let ingreso = [];
-let persona1 = [];
-let ingreso1 = [];
+let ingresos = [];
+let ingresoTotal = 0;
+let personas = [];
 let entrada = 0;
 
 class Persona{
@@ -17,7 +19,7 @@ class Persona{
         this.ingreso = ingreso;
     }
     hablar(){
-        console.log("Hola soy " + nombre + " y tengo " + edad + " años," + "con dni " + dni + ", tengo una deuda de " + deuda + " y un ingreso de " + ingreso);
+        console.log("Hola soy " + nombre + " y tengo " + edad + " años," + "con dni " + dni + ", tengo una deuda de " + deudaTotal + " y un ingreso de " + ingresoTotal);
     }
 //  Metodo para que nuestro objeto hable: this.hablar = function (){console.log("Hola soy " + nombre + " con dni " + dni + " y tengo una deuda de " + deuda + " con un ingreso de " + ingreso + " pesos.")}
 }
@@ -45,22 +47,37 @@ function nuevoIngreso(){
     ingreso = Number(prompt("Para agregar un ingreso pulse 1, de lo contrario 0."));
     if(ingreso == 1){
         ingreso = Number(prompt("Registre su ingreso"));
-        ingreso1 = new Ingreso(ingreso, nombre);
-        ingreso1.hablar();
+        ingresos.push(new Ingreso(ingreso, nombre));
+        for(const ingreso of ingresos){
+            ingreso.hablar();
+        }
+        ingresoTotal = ingresoTotal + ingreso;
     }else if(ingreso == 0){
         console.log("No registramos un ingreso");
     }else{
-        console.log("Hemos notado que ha ingresado un numero incorrecto. Por lo tanto no hemos registrado ningun ingreso");
-        dni = Number(prompt("Ingrese su dni"));
+        console.log("Hemos notado que ha ingresado un numero incorrecto. No se ha registrado ningun ingreso");
     }
-    persona1 = new Persona(nombre, dni, deuda, ingreso);
-    persona1.hablar();
+    personas.push(new Persona(nombre, dni, deuda, ingreso));
+    for(const persona of personas){
+        persona.hablar();
+    }
     Inicio();
 }
 
 function nuevaDeuda(){
-    deuda = Number(prompt("Ingrese su deuda"));
-    const deuda1 = new Deuda(deuda, nombre);
+    ingreso = Number(prompt("Para agregar una deuda pulse 1, de lo contrario 0."));
+    if (ingreso == 1){
+        deuda = Number(prompt("Ingrese su deuda"));
+        deudas.push(new Deuda(deuda, nombre));
+        for(const deuda of deudas){
+            deuda.hablar();
+        }
+        deudaTotal = deudaTotal + deuda;
+    }else if(ingreso == 0){
+        console.log("No registramos una deuda");
+    }else{
+        console.log("Hemos notado que ha ingresado un numero incorrecto. No se ha registrado ninguna deuda");
+    }
     nuevoIngreso();
 }
 
@@ -68,41 +85,28 @@ function verPersonas(){
     nombres.forEach(nom => {
         console.log(nom);
     });
-}
-function verEdades(){
-    edades.forEach(eda => {
-        console.log(eda);
-    });
+    Inicio(); 
 }
 
 function Inicio(){
-    let inicio = Number(prompt("Ingrese: 1. Para registrarse 2. Para ingresar una nueva deuda 3. Para ingresar un ingreso 4. Para ver todas las personas de esta sesion 5. Para ver los generos de las personas registradas 6. Para finalizar el programa"));
+    let inicio = Number(prompt("Ingrese: 1. Para registrarse 2. Para ingresar una nueva deuda 3. Para ingresar un ingreso 4. Para ver todas las personas de esta sesion 5. Para finalizar el programa"));
     switch(inicio){
         case 1: Registrar();break;
         case 2: nuevaDeuda();break;
         case 3: nuevoIngreso();break;
         case 4: verPersonas();break;
-        case 5: verEdades();break;
-        case 6: Finalizar();break;
+        case 5: Finalizar();break;
         default: Inicio(); console.log("Ingrese un numero correcto");break;
     }
 }
 
 function Registrar(){
     nombre = prompt("Ingrese su nombre");
-    edad = Number(prompt("Ingrese su edad"));
-    dni = Number(prompt("Ingrese su dni"));
-    deuda = Number(prompt("Si posee una deuda pulse 1, de lo contrario 0"));
     nombres.push(nombre);
+    edad = Number(prompt("Ingrese su edad"));
     edades.push(edad);
-    if (deuda == 1){
-        nuevaDeuda();
-    }else if(deuda > 1 || deuda < 0){
-        console.log("Ingrese un numero correcto. ");
-        Inicio();
-    }else{
-        nuevoIngreso();
-    }
+    dni = Number(prompt("Ingrese su dni"));
+    nuevaDeuda();
 }
 const promedioEdad = () => {let total = edades.reduce((acumulador, elemento) => acumulador + elemento, 0); return total / edades.length;}
 
